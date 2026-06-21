a8b5ff90190c6ffccf7d7d6271aaf8cf4a8e6724
MarceloMdx <marcelosaphira@gmail.com>
2026-06-20 23:01:37 -0300

    mudacas e atualizacoes feitas pelo Kenji

--- pokedex/App.tsx
+++ pokedex/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { PokemonProvider } from './src/contexts/PokemonContext';
-import PokemonListScreen from './src/screens/pokemon';
+import { View, Text, StyleSheet } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Pokemon from './src/app/screens/Pokemon'
+import PokemonDetails from './src/app/screens/PokemonDetails'
+
+export type RootStackParamList = {
+Pokemon: undefined;              
+PokemonDetails: { url: string };
+};
 
 export default function App() {
+const Stack = createStackNavigator<RootStackParamList>()
 return (
-<PokemonProvider>
-    <StatusBar style="auto" />
-    <PokemonListScreen />
-</PokemonProvider>
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Pokemon"
+                component={Pokemon}
+                options={{ title: 'Pokemon' }}
+            >
+            </Stack.Screen>
+            <Stack.Screen
+                name="PokemonDetails"
+                component={PokemonDetails}
+                options={{ title: 'Detalhe Pokemon' }}
+            >
+            </Stack.Screen>
+        </Stack.Navigator>
+    </NavigationContainer>
 );
 }
