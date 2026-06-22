cb622916cfffe25cbcfcd149008295af3500c9f1
pedrochuffart <pedropositivoone@gmail.com>
2026-06-22 12:59:11 -0300

    Criação do buscar Pokemon

--- pokedex/App.tsx
+++ pokedex/App.tsx
@@ -1,32 +1,42 @@
-import { View, Text, StyleSheet } from 'react-native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer } from '@react-navigation/native'
-import Pokemon from './src/app/screens/Pokemon'
-import PokemonDetails from './src/app/screens/PokemonDetails'
+    import { createStackNavigator } from '@react-navigation/stack'
+    import { NavigationContainer } from '@react-navigation/native'
 
-export type RootStackParamList = {
-Pokemon: undefined;              
-PokemonDetails: { url: string };
-};
+    import Pokemon from './src/app/screens/Pokemon'
+    import PokemonBusca from './src/app/screens/PokemonBusca'
+    import PokemonDetails from './src/app/screens/PokemonDetails'
 
-export default function App() {
-const Stack = createStackNavigator<RootStackParamList>()
-return (
-    <NavigationContainer>
+    export type RootStackParamList = {
+    Pokemon: undefined;
+    PokemonBusca: undefined;
+    PokemonDetails: { url: string };
+    };
+
+    const Stack = createStackNavigator<RootStackParamList>();
+
+    export default function App() {
+    return (
+        <NavigationContainer>
         <Stack.Navigator>
+
             <Stack.Screen
-                name="Pokemon"
-                component={Pokemon}
-                options={{ title: 'Pokemon' }}
-            >
-            </Stack.Screen>
+            name="PokemonBusca"
+            component={PokemonBusca}
+            options={{ title: 'Busca Pokemon' }}
+            />
+
             <Stack.Screen
-                name="PokemonDetails"
-                component={PokemonDetails}
-                options={{ title: 'Detalhe Pokemon' }}
-            >
-            </Stack.Screen>
+            name="Pokemon"
+            component={Pokemon}
+            options={{ title: 'Pokemon' }}
+            />
+
+            <Stack.Screen
+            name="PokemonDetails"
+            component={PokemonDetails}
+            options={{ title: 'Detalhe Pokemon' }}
+            />
+
         </Stack.Navigator>
-    </NavigationContainer>
-);
-}
+        </NavigationContainer>
+    );
+    }
